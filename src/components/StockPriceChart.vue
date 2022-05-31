30cb446a2ad34be7d9b36286f64655c75d786dc5
Ferran Sánchez <fersandom@gmail.com>
2022-05-31 18:19:31 +0200

    able to search, select a stock and display chart

---
+++ src/components/StockPriceChart.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { inject } from 'vue';
+import ApiStore from '../store/ApiStore';
+
+import { Line } from 'vue-chartjs'
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    PointElement,
+    CategoryScale,
+} from 'chart.js';
+ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    PointElement,
+    CategoryScale
+);
+
+inject[ApiStore];
+
+let chartData = {
+    labels: ApiStore.state.stockChartData.t,
+    datasets: [{
+        data: ApiStore.state.stockChartData.c,
+        label: `${ApiStore.state.selectedStock} Price`,
+
+    }]
+};
+let chartOptions = {
+    responsive: true
+};
+
+
+
+</script>
+
+<template>
+    <Line :chart-options="chartOptions" :chart-data="chartData" />
+</template>
+
+<style>
+</style>
